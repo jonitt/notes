c90225fd50e7a70ed46eca5f8a0dda93efe04550
jonitt <jonit44@gmail.com>
2020-05-24 02:36:42 +0800

    U readme

--- src/frontend/redux/rootSaga.js
+++ src/frontend/redux/rootSaga.js
@@ -1,5 +1,6 @@
 import { fork, call, put, spawn, take, select, all } from 'redux-saga/effects';
 import { watchApp } from './app';
+
 export default function* rootSaga() {
   yield all([watchApp()]);
 }
